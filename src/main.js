04fd731b8a667c9aaf6ae18ef9da65a6e29f23c1
adam kassir <adam.kassir@gmail.com>
2022-06-03 09:27:41 +0200

    home page ok avec menu, title et plats

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+// import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+
 
 Vue.config.productionTip = false;
 
 new Vue({
+  el : '#app',
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <span>
+    <HomeHero/>
+    <HomeDetails/>
+    <HomePlans/>
+  </span>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomeHero from "@/components/HomeHero";
+import HomeDetails from "@/components/HomeDetails";
+import HomePlans from "@/components/HomePlans";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
-  },
+    HomeHero,
+    HomeDetails,
+    HomePlans
+},
 };
 </script>
